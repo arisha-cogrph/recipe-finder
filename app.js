6879b463ec31bd7d9718ee4a87f31c1a43d401fa
arisha-cogrph <arisha_satari@co-graph.com>
2025-03-04 16:57:40 +0900

    add js and html complete

---
+++ app.js
@@ -0,0 +1,9 @@
+const apiKey = "8c17bec049ff49e19cc5cc08b58ee5f5";
+//above is the API key that you get from the spoonacular API website. you cant use their API if you dont sign up and
+//add this key
+
+const searchForm = document.getElementById('searchForm');
+const ingredientInput = document.getElementById('ingredientInput');
+const recipeResults = document.getElementById('recipe-results');
+const recipeDetail = document.getElementById('recipe-detail');
+
